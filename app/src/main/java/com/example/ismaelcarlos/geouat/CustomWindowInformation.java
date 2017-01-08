43cd8f36a2bc6870e08a35d651af9a5295a38a82
carlosismael4 <carlosismael4@hotmail.com>
2017-01-07 18:02:28 -0600

    Quitar y agregar markers del mapa por medio del drawerlayout

---
+++ app/src/main/java/com/example/ismaelcarlos/geouat/CustomWindowInformation.java
@@ -0,0 +1,8 @@
+package com.example.ismaelcarlos.geouat;
+
+/**
+ * Created by Ismael Carlos on 07/01/2017.
+ */
+
+public class CustomWindowInformation {
+}
--- app/src/main/java/com/example/ismaelcarlos/geouat/UATMap.java
+++ app/src/main/java/com/example/ismaelcarlos/geouat/UATMap.java
@@ -1,9 +1,19 @@
 package com.example.ismaelcarlos.geouat;
 
 import android.graphics.Point;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,13 +21,23 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class UATMap extends AppCompatActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+
+public class UATMap extends AppCompatActivity implements OnMapReadyCallback,
+        NavigationView.OnNavigationItemSelectedListener{
 
     private GoogleMap mMap;
+    private boolean flagFacultary = false;
+    private boolean flagGym = false;
+    private List<Marker> markerFacultary;
+    private List<Marker> markerOffices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +47,26 @@ public class UATMap extends AppCompatActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 
@@ -42,12 +82,14 @@ public class UATMap extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        markerFacultary = new ArrayList<>();
+        markerOffices = new ArrayList<>();
         // Add a marker in UAT tamaulipas and move the camera
         LatLng UAT = new LatLng(23.7157506,-99.1519597);
-        mMap.addMarker(new MarkerOptions().position(UAT).title("4toPiso"));
+       /* mMap.addMarker(new MarkerOptions().position(UAT).title("4toPiso"));*/
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UAT,17));
         mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this,R.raw.map_style));
-        addFacultary();
+       // addFacultary();
         position();
     }
 
@@ -69,27 +111,124 @@ public class UATMap extends AppCompatActivity implements OnMapReadyCallback {
         });
     }
 
-    public void addFacultary(){
+    public void addMarkersFacultary(){
+
+        Marker comercio =
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(23.71640728,-99.1511106))
                 .title("Comercio"));
+        Marker cellap =
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(23.7172535726,-99.1512420))
                 .title("CELLAP"));
-        mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(23.7172572571,-99.1521712))
-                .title("Trabajo Social"));
+        Marker ciencias =
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(23.7171293570,-99.14989806))
                 .title("Ciencias"));
-        mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(23.71822404,-99.15269393))
-                .title("Gimnacio Multi"));
+        Marker fic =
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(23.71552169,-99.15311230))
                 .title("FIC"));
+        Marker trabajoSocial =
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.7172572571,-99.1521712))
+                .title("Trabajo Social"));
+
+        markerFacultary.add(comercio);
+        markerFacultary.add(cellap);
+        markerFacultary.add(ciencias);
+        markerFacultary.add(fic);
+        markerFacultary.add(trabajoSocial);
+        flagFacultary = true;
+    }
+    public void removeMarkers(List<Marker> markers){
+        for(int i = 0; i < markers.size(); i++){
+            markers.get(i).remove();
+        }
+    }
+    public void addMarkersOffices(){
+
+        Marker centroExcelencia =
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(23.7162351,-99.1522213))
-                .title("Centro de Excelencia"));
+                .title("Centro de Excelencia"))
+                .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.));
+        Marker multi =
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.71822404,-99.15269393))
+                .title("Gimnacio Multi"));
+        Marker fourFloor =
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(23.7157506,-99.1519597))
+                .title("Cuarto Piso"));
+        markerOffices.add(centroExcelencia);
+        markerOffices.add(multi);
+        markerOffices.add(fourFloor);
+        flagGym = true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.facultary) {
+            if(flagFacultary){
+                flagFacultary = false;
+                removeMarkers(markerFacultary);
+            }else
+                addMarkersFacultary();
+        } else if (id == R.id.gym) {
+            if(flagGym){
+                flagGym = false;
+                removeMarkers(markerOffices);
+            }else
+                addMarkersOffices();
+        } else if (id == R.id.offices) {
+          //  Toast.makeText(this,"slides",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.localdining) {
+           // Toast.makeText(this,"administrador",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_share) {
+           // Toast.makeText(this,"Compartir",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_send) {
+          //  Toast.makeText(this,"Enviar",Toast.LENGTH_LONG).show();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //drawer.
+        //drawer.clearAnimation();
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
